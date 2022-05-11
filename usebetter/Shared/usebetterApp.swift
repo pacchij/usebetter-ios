6d1873fc20697e5b9d2fcfeba94c5e1524ef8e8d
Prashanth Jaligama <prashanthjaligama@Prashanths-MacBook-Pro-2.local>
2022-05-11 13:21:58 -0700

    initiala app

---
+++ usebetter/Shared/usebetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  usebetterApp.swift
+//  Shared
+//
+//  Created by Prashanth Jaligama on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct usebetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
